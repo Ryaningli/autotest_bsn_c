1ea0b8c2d7814cadedffb763a03e37dd538c2062
ryan <1669971502@qq.com>
2021-01-17 20:57:04 +0800

    上班上班

--- page/page_tem.py
+++ page/page_tem.py
@@ -1,13 +1,5 @@
-import random
-from time import sleep
-import page
 from base.base import Base
 
+a = Base(1)
 
-class PageTem(Base):
-    def page_get(self):
-        self.driver.get('http://192.168.0.158/service/uploadservice?type=1&appTypeName=Fabric-secp256r1-1.4.3&appInfoId=12327&frameType=fabric&payType=1')
-        self.driver.refresh()
-        sleep(2)
-        js = 'document.querySelector("#editor>div:nth-child(2)>div>p").innerText="这是服务的描述"'
-        self.driver.execute_script(js)
+a.base_get_image(shot_type='0')
